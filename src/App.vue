f2a3ddcb78b1cc34f2a588980bd137acd34ceb02
Jemster07 <emelie.kerek@gmail.com>
2023-08-28 11:52:36 -0400

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <button type="button" class="btn" v-on:click="showModal">
+      Open Modal!
+    </button>
+    <Modal v-show="isModalVisible" v-on:close="closeModal">
+      <template v-slot:header>
+        This is a new modal header.
+      </template>
+
+      <template v-slot:body>
+        This is a new modal body.
+      </template>
+
+      <template v-slot:footer>
+        This is a new modal footer.
+      </template>
+    </Modal>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal,
+  },
+  data() {
+    return {
+      isModalVisible: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
   }
 }
 </script>
